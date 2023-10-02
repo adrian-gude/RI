e35b076a819bd479b5aee6dcfd88db326612c960
SamuelDeus <samuel.deus@udc.es>
2023-10-02 19:55:06 +0200

    Cambio del spider de hoteles. Ahora se utiliza BeautifulSoup

--- tripadvisor/tripadvisor/spiders/hotels_spider.py
+++ tripadvisor/tripadvisor/spiders/hotels_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from bs4 import BeautifulSoup
   
   
 class HotelsSpider(scrapy.Spider):
@@ -10,14 +11,47 @@ class HotelsSpider(scrapy.Spider):
     # list should be named 'start_urls' only
     start_urls = [
         'https://www.tripadvisor.es/Hotels',
-        #'https://www.tripadvisor.es/Search?searchSessionId=000c5c686c7b20a0.ssid&ssrc=h&q=Espa%C3%B1a&sid=485E16AFF3684177A1546710A6AF00A31695664100074&blockRedirect=true&geo=1&rf=2',
-        #'https://www.tripadvisor.es/Search?searchSessionId=000c5c686c7b20a0.ssid&ssrc=h&q=Espa%C3%B1a&sid=485E16AFF3684177A1546710A6AF00A31695664100074&blockRedirect=true&geo=1&rf=3&o=30',
-        #'https://www.tripadvisor.es/Search?searchSessionId=000c5c686c7b20a0.ssid&ssrc=h&q=Espa%C3%B1a&sid=485E16AFF3684177A1546710A6AF00A31695664100074&blockRedirect=true&geo=1&rf=4&o=60',
+        #'https://www.tripadvisor.es/Hotels-g187506-Galicia-Hotels.html',
+        #'https://www.tripadvisor.es/Hotels-g187506-oa30-Galicia-Hotels.html',
     ]
-  
-    def parse(self, response):
 
-        for hotel in response.css('div.ui_shelf_item_detail'):
+    def parse(self, response):
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        lista_hoteles = soup.find_all('div', class_='ui_shelf_item_detail')
+
+        for hotel in lista_hoteles:
+            #Coger el nombre del hotel
+            name = hotel.find('a').get_text()
+
+            #Coger el número de ratings
+            ratingsNumberString = hotel.find_all('span', class_='reviewCount')
+            rating = ""
+            if len(ratingsNumberString) > 0:
+                rating = ratingsNumberString[0].get_text()
+                rating = rating.replace('.', '')
+                rating = rating.split(' ')
+                for ratingString in rating:
+                    if ratingString.isnumeric():
+                        rating = int(ratingString)
+                        break
+
+            #Coger la dirección del hotel
+            address = hotel.find_all('div', class_='item tags')
+            if len(address) > 0:
+                address = address[0].get_text()
+                
+
+            yield {
+                'dataType': 'spainHotels',
+                'data': {
+                    'name': name,
+                    'ratings': rating,
+                    'address': address,
+                }
+            }
+
+        """for hotel in response.css('div.ui_shelf_item_detail'):
 
             #Se obtiene el número de ratings
             ratingsNumberString = hotel.css('span.reviewCount::text').get()
@@ -36,6 +70,8 @@ class HotelsSpider(scrapy.Spider):
                     'ratings': ratingsNumber,
                     'address': hotel.css('div.item.tags::text').get(),
                 }
-            }
+            }"""
 
     #scrapy crawl hotels -O hotels.json
+
+    #https://www.tripadvisor.es/Hotels-g187506-oa330-Galicia-Hotels.html
