24641c78da01d58d0a0be32bd2783c4ce83ac729
SamuelDeus <samuel.deus@udc.es>
2023-10-29 00:22:02 +0200

    Modificado spyder para coger url, almacenar todo como strings y traducir ya la comunidad.
    Además, se actualiza el hotels.json resultante de ejecutar el spyder.

--- tripadvisor/tripadvisor/spiders/hotels_spider.py
+++ tripadvisor/tripadvisor/spiders/hotels_spider.py
@@ -12,8 +12,8 @@ SERVICIOS = ['Aparcamiento público de pago cerca', 'Wifi', 'Gimnasio / Sala de 
 IDIOMAS = ['Español', 'Inglés', 'Francés', 'Italiano', 'Portugués']
 
 class HotelItem(scrapy.Item):
-    comunidad = scrapy.Field(output_processor=TakeFirst())
     nombre = scrapy.Field(output_processor=TakeFirst())
+    comunidad = scrapy.Field(output_processor=TakeFirst())
     precio = scrapy.Field(output_processor=TakeFirst())
     comunidad = scrapy.Field(output_processor=TakeFirst())
     localizacion = scrapy.Field(output_processor=TakeFirst())
@@ -22,6 +22,8 @@ class HotelItem(scrapy.Item):
     categoria = scrapy.Field(output_processor=TakeFirst())
     idiomas = scrapy.Field()
     servicios = scrapy.Field()
+    url = scrapy.Field(output_processor=TakeFirst())
+    hotel_completion = scrapy.Field()
 
 class HotelsSpider(scrapy.Spider):
     name = "hotels" 
@@ -31,37 +33,37 @@ class HotelsSpider(scrapy.Spider):
 
     BASE_URL = 'https://www.tripadvisor.es/Hotels-g{}-{}-Hotels.html'
     COMUNIDADES = [
-       ('187506', 'Galicia'),
-       ('187449', 'Asturias'),
-       ('187483', 'Cantabria'),
-       ('187453', 'Basque_Country'),
-       ('187519', 'Navarra'),
-       ('187444', 'Aragon'),
-       #('187496', 'Catalonia'),
-       ('187490', 'Castile_and_Leon'),
-       ('187511', 'La_Rioja'),
-       ('187514', 'Madrid'),
-       ('187505', 'Extremadura'),
-       ('187485', 'Castile_La_Mancha'),
-       ('187521', 'Valencian_Community'),
-       ('187518', 'Murcia'),
-       #('187428', 'Andalucia'),
-       ('187459', 'Balearic_Islands'),
-       ('187466', 'Canary_Islands')
+       ('187506', 'Galicia', 'Galicia'),
+       ('187449', 'Asturias', 'Asturias'),
+       ('187483', 'Cantabria', 'Cantabria'),
+       ('187453', 'Basque_Country', 'Pais Vasco'),
+       ('187519', 'Navarra', 'Navarra'),
+       ('187444', 'Aragon', 'Aragon'),
+       #('187496', 'Catalonia', 'Cataluña'),
+       ('187490', 'Castile_and_Leon', 'Castilla y Leon'),
+       ('187511', 'La_Rioja', 'La Rioja'),
+       ('187514', 'Madrid', 'Madrid'),
+       ('187505', 'Extremadura', 'Extremadura'),
+       ('187485', 'Castile_La_Mancha', 'Castilla La Mancha'),
+       ('187521', 'Valencian_Community', 'Comunidad Valenciana'),
+       ('187518', 'Murcia', 'Murcia'),
+       #('187428', 'Andalucia', 'Andalucia'),
+       ('187459', 'Balearic_Islands', 'Islas Baleares'),
+       ('187466', 'Canary_Islands', 'Islas Canarias')
     ]
     PAGES_TO_SCRAPE = 14 # Número de páginas a scrapear por comunidad
 
     def start_requests(self):
-        for code, comunidad in self.COMUNIDADES:
+        for code, comunidad, nombreComunidad in self.COMUNIDADES:
             start_url = self.BASE_URL.format(code, comunidad)
-            yield scrapy.Request(start_url, callback=self.parse, cb_kwargs={'comunidad': comunidad})
+            yield scrapy.Request(start_url, callback=self.parse, cb_kwargs={'comunidad': comunidad, 'nombreComunidad': nombreComunidad})
 
-    def parse(self, response, comunidad):
+    def parse(self, response, comunidad, nombreComunidad):
         for page in range(self.PAGES_TO_SCRAPE):
             url = response.url.replace(f'-Hotels.html', f'-oa{page * 30}-{comunidad}-Hotels.html')
-            yield scrapy.Request(url, callback=self.parse_hotel, cb_kwargs={'comunidad': comunidad})
+            yield scrapy.Request(url, callback=self.parse_hotel, cb_kwargs={'nombreComunidad': nombreComunidad})
 
-    def parse_hotel(self, response, comunidad):
+    def parse_hotel(self, response, nombreComunidad):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Obtén los enlaces a los hoteles en la página inicial
@@ -73,9 +75,10 @@ class HotelsSpider(scrapy.Spider):
                 hotel_url = response.urljoin(hotel_url)
 
             if hotel_url:
-                yield scrapy.Request(url=hotel_url, callback=self.parse_hotel_details, cb_kwargs={'comunidad': comunidad})
+                yield scrapy.Request(url=hotel_url, callback=self.parse_hotel_details, cb_kwargs={'comunidad': nombreComunidad})
 
     def parse_hotel_details(self, response, comunidad):
+        url = response.url
         sel = Selector(response)
         item = ItemLoader(HotelItem(), sel)
 
@@ -103,26 +106,33 @@ class HotelsSpider(scrapy.Spider):
                 raise AttributeError
             precio = precio.replace('€', '').replace(',', '.').replace(' ', '')
             precio = float(precio)
+            precio = str(precio)
 
             field_count += 1
             localizacion = soup.find('span', class_='fHvkI PTrfg').get_text()
 
             field_count += 1
             n_opiniones = soup.find('span', class_='qqniT').get_text()
-            n_opiniones = int(n_opiniones.replace('.', '').split(' ')[0])
+            n_opiniones = n_opiniones.replace('.', '').split(' ')[0]
+            #n_opiniones = int(n_opiniones)
 
             field_count += 1
             puntuacion = soup.find('span', class_='uwJeR P').get_text()
             if puntuacion is not None and puntuacion != '':
-                puntuacion = float(puntuacion.replace(',', '.'))
+                puntuacion = puntuacion.replace(',', '.')
+                puntuacion = float(puntuacion)
+                puntuacion = str(puntuacion)
 
             field_count += 1
             try:
                 categoria = soup.find('svg', class_='JXZuC d H0')['aria-label'][0]
-                if categoria is not None and categoria != '':
-                    categoria = int(categoria)
+                #if categoria is not None and categoria != '':
+                    #categoria = int(categoria)
+                if categoria is None or categoria == '':
+                    raise AttributeError
             except:
-                categoria = -1
+                #categoria = -1
+                categoria = "-1"
 
             field_count += 1
             idiomas = []
@@ -142,6 +152,8 @@ class HotelsSpider(scrapy.Spider):
                 if servicio in SERVICIOS:
                     servicios.append(servicio)
 
+            hotel_completion = idiomas + servicios + [comunidad]
+
             # Añade los valores a los campos
             item.add_value('comunidad', comunidad)
             item.add_value('nombre', nombre)
@@ -152,6 +164,8 @@ class HotelsSpider(scrapy.Spider):
             item.add_value('categoria', categoria)
             item.add_value('idiomas', idiomas)
             item.add_value('servicios', servicios)
+            item.add_value('url', url)
+            item.add_value('hotel_completion', hotel_completion)
 
             # Obtiene los valores de los campos, o None si no existen
             comunidad = item.get_collected_values('comunidad')[0] if item.get_collected_values('comunidad') else None
@@ -163,6 +177,8 @@ class HotelsSpider(scrapy.Spider):
             categoria = item.get_collected_values('categoria')[0] if item.get_collected_values('categoria') else None
             idiomas = item.get_collected_values('idiomas')
             servicios = item.get_collected_values('servicios')
+            url = item.get_collected_values('url')[0] if item.get_collected_values('url') else None
+            hotel_completion = item.get_collected_values('hotel_completion')
 
             # Crea un diccionario con los valores individuales
             hotel_data = {
@@ -175,6 +191,8 @@ class HotelsSpider(scrapy.Spider):
                 'categoria': categoria,
                 'idiomas': idiomas,
                 'servicios': servicios,
+                'url': url,
+                'hotel_completion': hotel_completion
             }
 
             yield hotel_data # Devuelve el diccionario con los valores
@@ -184,4 +202,25 @@ class HotelsSpider(scrapy.Spider):
             if MOSTRAR_WARNINGS:
                 self.logger.warning(f'No se pudo encontrar información en {response.url} para el campo {fields[field_count]}')
 
+
+#Comando para ejecutar el spyder:
     #scrapy crawl hotels -O hotels.json
+
+### Pasos posteriores a la ejecución del spyder con objetivo de construír un archivo válido para incluír en el índice:
+# 1. Abrir el archivo hotels.json y eliminar la primera y última línea, que son "[" y "]"
+# 2. Eliminar las comas que hay al final de cada línea y que están separando cada hotel
+# 3. Añadir "{create: {}}" una vez por cada hotel
+# 4. Añadir/comprobar que haya una línea en blanco al final del archivo
+
+# Así, al final pasamos de algo como esto:
+#[
+#{"comunidad": "Pais Vasco", "nombre": "nombre1", "precio": "84.0", "localizacion": "Calle X", "n_opiniones": "109", "puntuacion": "4.5", "categoria": "2", "idiomas": ["Español", "Inglés"], "servicios": ["Habitaciones de no fumadores"], "url":"ejemplo1", "hotel_completion": ["Español", "Inglés", "Habitaciones de no fumadores", "Pais Vasco"]},
+#{"comunidad": "Pais Vasco", "nombre": "nombre2, "precio": "84.0", "localizacion": "Calle Y", "n_opiniones": "109", "puntuacion": "4.5", "categoria": "2", "idiomas": ["Español", "Inglés"], "servicios": ["Hotel de no fumadores"], "url":"ejemplo2", "hotel_completion": ["Español", "Inglés", "Hotel de no fumadores", "Pais Vasco"]}
+#}
+
+# A algo como esto:
+#{"create": {}}
+#{"comunidad": "Pais Vasco", "nombre": "nombre1", "precio": "84.0", "localizacion": "Calle X", "n_opiniones": "109", "puntuacion": "4.5", "categoria": "2", "idiomas": ["Español", "Inglés"], "servicios": ["Habitaciones de no fumadores"], "url":"ejemplo1", "hotel_completion": ["Español", "Inglés", "Habitaciones de no fumadores", "Pais Vasco"]}
+#{create: {}
+# {"comunidad": "Pais Vasco", "nombre": "nombre2, "precio": "84.0", "localizacion": "Calle Y", "n_opiniones": "109", "puntuacion": "4.5", "categoria": "2", "idiomas": ["Español", "Inglés"], "servicios": ["Hotel de no fumadores"], "url":"ejemplo2", "hotel_completion": ["Español", "Inglés", "Hotel de no fumadores", "Pais Vasco"]}
+# 
