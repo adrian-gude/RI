3af19be37cb1f62eb17a4aa9a5897d20e1d8dabf
SamuelDeus <samuel.deus@udc.es>
2023-11-10 18:28:00 +0100

    avances en el estilo

--- BuscaTuHotel/src/index.js
+++ BuscaTuHotel/src/index.js
@@ -31,147 +31,165 @@ class Main extends React.Component {
         app="hotels" // Nombre de tu aplicación de Elasticsearch
         url="http://localhost:9200" // URL de tu instancia de Elasticsearch
       >
-        <div style={{textAlign: 'center', marginBottom:'3%' }}>
-          Busca tu Hotel
+        <div style={{ textAlign: 'center'}}>
+          <div className='header'>
+              <h1>Busca tu Hotel</h1>
+          </div>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+        
+        <div style={{ display: 'flex', flexDirection: 'row'}} className='colorFondoPrincipal'>
           <div
             style={{
               display: 'flex',
               flexDirection: 'column',
               width: '24%',
               margin: '10px',
+              marginTop: '2%',
             }}
           >
-            <MultiDropdownList
-              componentId="comunidad_multidropselector"
-              compoundClause="filter"
-              dataField="comunidad"
-              title="Comunidades"
-              sortBy="asc"
-              showCount={true}
-              placeholder="Comunidades"
-              showFilter={true}  
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }} 
-            />
-            <RangeInput
-              componentId="precio_slider_input"
-              dataField="precio"
-              title="Precio"
-              range={{
-                start: 0,
-                end: 1500,
-              }}
-              rangeLabels={{
-                start: '0',
-                end: '1500',
-              }}
-              showHistogram={false}
-              showFilter={true}
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }}
-            />
-            <RatingsFilter
-              componentId="puntuacion_ratingselector"
-              dataField="puntuacion"
-              title="Puntuacion"
-              icon={<CircleIcon htmlColor="seagreen  " />}
-              dimmedIcon={<CircleIcon htmlColor='grey'/>}
-              data={[
-                { start: 1, end: 5, label: '1 o más' },
-                { start: 2, end: 5, label: '2 o más' },
-                { start: 3, end: 5, label: '3 o más' },
-                { start: 4, end: 5, label: '4 o más' },
-                { start: 5, end: 5, label: '5' }
-              ]}
-              /*defaultValue={{
-                start: 0,
-                end: 5,
-              }}*/
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }}
-            />
-            <MultiRange
-              componentId="categoria_multiselector"
-              compoundClause="filter"
-              dataField="categoria"
-              data={[
-                //{ start: 1, end: 1, label: <StarIcon htmlColor="#F2B203"/> },
-                { start: 1, end: 1, label: '1 estrella' },
-                { start: 2, end: 2, label: '2 estrellas' },
-                { start: 3, end: 3, label: '3 estrellas'  },
-                { start: 4, end: 4, label: '4 estrellas'  },
-                { start: 5, end: 5, label: '5 estrellas'  },
-                //{ start: 5, end: 5, label: <Rating name="read-only" value={5} readOnly /> },
-              ]}
-              title="Categoria"
-              showCheckbox={true}
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }}
-            />
-            <MultiDropdownList
-              componentId="idiomas_multidropselector"
-              compoundClause="filter"
-              dataField="idiomas"
-              title="Idiomas"
-              //size={100}
-              sortBy="count"
-              showCount={true}
-              placeholder="Idiomas"
-              showFilter={true}
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }} 
-            />
-            <MultiDropdownList
-              componentId="servicios_multidropselector"
-              compoundClause="filter"
-              dataField="servicios"
-              title="Servicios"
-              //size={100}
-              sortBy="count"
-              showCount={true}
-              placeholder="Servicios"
-              showFilter={true}
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }}  
-            />
+            <div style={{marginBottom:'5%'}}>
+              <MultiDropdownList
+                componentId="comunidad_multidropselector"
+                compoundClause="filter"
+                dataField="comunidad"
+                title="Comunidades autónomas"
+                sortBy="asc"
+                showCount={true}
+                placeholder="Comunidades"
+                showFilter={true}  
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }} 
+              />
+            </div>
+            <div style={{marginBottom:'5%'}}>
+              <RangeInput
+                componentId="precio_slider_input"
+                dataField="precio"
+                title="Precio"
+                range={{
+                  start: 0,
+                  end: 1500,
+                }}
+                defaultValue={{
+                  start: 50,
+                  end: 800,
+                }}
+                rangeLabels={{
+                  start: '0',
+                  end: '1500',
+                }}
+                showHistogram={false}
+                showFilter={true}
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }}
+              />
+            </div>
+            <div style={{marginBottom:'5%'}}>
+              <RatingsFilter
+                componentId="puntuacion_ratingselector"
+                dataField="puntuacion"
+                title="Puntuación"
+                icon={<CircleIcon htmlColor="seagreen  " />}
+                dimmedIcon={<CircleIcon htmlColor='grey'/>}
+                data={[
+                  { start: 1, end: 5, label: '1 o más' },
+                  { start: 2, end: 5, label: '2 o más' },
+                  { start: 3, end: 5, label: '3 o más' },
+                  { start: 4, end: 5, label: '4 o más' },
+                  { start: 5, end: 5, label: '5' }
+                ]}
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }}
+              />
+            </div>
+            <div style={{marginBottom:'5%'}}>
+              <MultiRange
+                componentId="categoria_multiselector"
+                compoundClause="filter"
+                dataField="categoria"
+                data={[
+                  //{ start: 1, end: 1, label: <StarIcon htmlColor="#F2B203"/> },
+                  { start: 1, end: 1, label: '1 estrella' },
+                  { start: 2, end: 2, label: '2 estrellas' },
+                  { start: 3, end: 3, label: '3 estrellas'  },
+                  { start: 4, end: 4, label: '4 estrellas'  },
+                  { start: 5, end: 5, label: '5 estrellas'  },
+                  //{ start: 5, end: 5, label: <Rating name="read-only" value={5} readOnly /> },
+                ]}
+                title="Categoría"
+                showCheckbox={true}
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }}
+              />
+            </div>
+            <div style={{marginBottom:'5%'}}>
+              <MultiDropdownList
+                componentId="idiomas_multidropselector"
+                compoundClause="filter"
+                dataField="idiomas"
+                title="Idiomas"
+                //size={100}
+                sortBy="count"
+                showCount={true}
+                placeholder="Idiomas"
+                showFilter={true}
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }} 
+              />
+            </div>
+            <div style={{marginBottom:'5%'}}>
+              <MultiDropdownList
+                componentId="servicios_multidropselector"
+                compoundClause="filter"
+                dataField="servicios"
+                title="Servicios"
+                //size={100}
+                sortBy="count"
+                showCount={true}
+                placeholder="Servicios"
+                showFilter={true}
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }}  
+              />
+            </div>
           </div>
           <div style={{ display: 'flex', flexDirection: 'column', width: '72%', marginLeft: '1.4%' }}>
-            <DataSearch
-              componentId="searchbox"
-              dataField={[
-                {
-                  field: 'nombre',
-                  weight: 3,
-                },
-                {
-                  field: 'localizacion',
-                  weight: 1,
-                }
-              ]}
-              placeholder="Buscar hotel"
-              autosuggest={true}
-              //enableRecentSuggestions={true}
-              //enablePopularSuggestions={true}
-              //enablePredictiveSuggestions={true}
-              react={{
-                and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
-                 'idiomas_multidropselector', 'servicios_multidropselector'],
-              }}
-            />
+            <div style={{ marginTop: '2%' }}>
+              <DataSearch
+                componentId="searchbox"
+                dataField={[
+                  {
+                    field: 'nombre',
+                    weight: 3,
+                  },
+                  {
+                    field: 'localizacion',
+                    weight: 1,
+                  }
+                ]}
+                placeholder="Busca un hotel"
+                autosuggest={true}
+                //enableRecentSuggestions={true}
+                //enablePopularSuggestions={true}
+                //enablePredictiveSuggestions={true}
+                react={{
+                  and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
+                  'idiomas_multidropselector', 'servicios_multidropselector'],
+                }}
+              />
+            </div>
             <ReactiveList
               componentId="results"
               dataField="_score"
@@ -179,19 +197,25 @@ class Main extends React.Component {
               //sortBy='desc'
               sortOptions={[
                 {
-                  label: "Desc",
+                  label: "-", //Ordenación por defecto, que es por la puntuación
+                  dataField: "_score",
+                  sortBy: "desc"
+                },
+                {
+                  label: "Puntuación descendente", //Ordenación por la puntuación del hotel de forma descendente
                   dataField: "puntuacion",
                   sortBy: "desc"
                 },
                 {
-                  label: "Asc",
+                  label: "Puntuación ascendente", //Ordenación por la puntuación del hotel de forma ascendente
                   dataField: "puntuacion",
                   sortBy: "asc"
                 }
               ]}
-              defaultSortOption='Desc'
+              defaultSortOption='-'
               stream={true}
               pagination={true}
+              paginationAt='bottom'
               react={{
                 and: ['searchbox', 'comunidad_multidropselector', 'categoria_multiselector', 'precio_slider_input', 'puntuacion_ratingselector',
                  'idiomas_multidropselector', 'servicios_multidropselector'],
@@ -199,58 +223,57 @@ class Main extends React.Component {
               loader={<div>Cargando...</div>}
               showResultStats={true} //Mostrar estadísticas de resultados
               render={({ data }) => (
-                /*<ReactiveList.ResultListWrapper>
-                  {data.map((item) => (
-                    <ResultCard key={item._id}>
-                      <ResultCard.Image src={item.imageUrl} />
-                      <ResultCard.Title
-                        dangerouslySetInnerHTML={{
-                          __html: item.nombre,
-                        }}
-                      />
-                      <ResultCard.Description>
-                        {item.comunidad + ' ' + '*'.repeat(item.puntuacion)+'\n'}
-                        {item.n_opiniones}
-                      </ResultCard.Description>
-                    </ResultCard>
-                  ))}
-                </ReactiveList.ResultListWrapper>*/
                 <ReactiveList.ResultListWrapper>
                   {data.map(item => (
-                    <ResultList key={item._id}>
-                      <ResultList.Image src={item.imageUrl} />
-                      <ResultList.Content>
-                        <ResultList.Title>
-                        <a href={item.url} target="_blank" rel="noopener noreferrer" className="no-link-style">
-                        <div className="book-title" dangerouslySetInnerHTML={{ __html: item.nombre }} />
-                      </a>
-                        </ResultList.Title>
-                        <ResultList.Description>
-                          <div className="flex column justify-space-between">
-                            <div>
-                              <span className="pub-year">
-                                {item.precio}€
-                              </span>
-                              <div className="ratings-list flex align-center">
-                                <span className="stars">
-                                  {item.categoria} estrellas
-                                </span>
+                    <div className='hotelWrapper'>
+                      <ResultList key={item._id}>
+                        <div className='column-img'>
+                          <img src={item.imageUrl} className='result-image'/>
+                        </div>
+                        <div className='column'>
+                        <ResultList.Content>
+                          <ResultList.Title>
+                            <div className='grid-container'>
+                              <div className='column'>
+                                <a href={item.url} target="_blank" rel="noopener noreferrer" className="no-link-style">
+                                  <div dangerouslySetInnerHTML={{ __html: item.nombre }} />
+                                </a>
                               </div>
-                              <div className="ratings-list flex align-center">
-                                <span className="avg-rating">
-                                  {item.puntuacion}/5 - {item.n_opiniones} opiniones
+                              <div className='column priceText'>
+                                <span>
+                                      {item.precio}€
                                 </span>
                               </div>
                             </div>
-                            <div>
-                                <span className="authors-list">
-                                  {item.localizacion}
+                          </ResultList.Title>
+                          <ResultList.Description>
+                            <div className='grid-container'>
+                              <div className='column'>
+                                <span>
+                                  {item.categoria} estrellas
                                 </span>
                               </div>
-                          </div>
-                        </ResultList.Description>
-                      </ResultList.Content>
-                    </ResultList>
+                              <div className='column'>
+                                <span>
+                                  {item.puntuacion} - {item.n_opiniones} opiniones
+                                </span>
+                              </div>
+                            </div>
+                              <div className='column'>
+                                  <span>
+                                    {item.comunidad} - {item.localizacion}
+                                  </span>
+                              </div>
+                              <div className='column'>
+                                  <span>
+                                    {item.servicios}
+                                  </span>
+                              </div>
+                          </ResultList.Description>
+                        </ResultList.Content>
+                        </div>
+                      </ResultList>
+                    </div>
                   ))}
                 </ReactiveList.ResultListWrapper>
               )}
